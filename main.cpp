d0779fa77a05e584650309c1713026edebe23a6d
pawel47 <pawelwierzbicki@rocketmail.com>
2023-08-08 21:05:35 +0000

    recursive
    Signed-off-by: pawel47 <pawelwierzbicki@rocketmail.com>

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mbed.h"
+
+
+
+int fib(int n){
+    if(n == 1)
+        return 0;
+    if(n == 2)
+        return 1;
+    else
+        return (fib(n-1) + fib(n-2));
+
+
+}
+
+int main()
+{
+    printf("Lab 2 program \r\n");
+    int max_term = 10;
+
+    for(int i=1; i<=max_term; i++){
+        printf("%d, ", fib(i));
+    }
+    printf("\r\n");
+    while (true) {
+
+    }
+}
